93c5b50bae69c530db3c2e367e2bdfc04a1b8b27
alnah <alexis.nahan@gmail.com>
2024-07-23 20:47:37 -0300

    feat(products): add logic to `uploadProductImage` on the server in `./public/uploads`

--- src/controllers/uploadsController.js
+++ src/controllers/uploadsController.js
@@ -1,7 +1,22 @@
+const path = require("path");
+const fs = require("fs");
+
 const { StatusCodes } = require("http-status-codes");
 
 const uploadProductImage = async (req, res, next) => {
-  res.send("upload product image");
+  const productImage = req.files.image;
+
+  const uploadsDir = path.join(__dirname, "../../public/uploads");
+  const imagePath = path.join(uploadsDir, productImage.name.toLowerCase());
+
+  if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir, { recursive: true });
+  }
+
+  await productImage.mv(imagePath);
+  res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name.toLowerCase()}` } });
 };
 
 module.exports = { uploadProductImage };
