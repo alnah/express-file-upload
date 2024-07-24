a371b5d5d0f4639cd7f3a1a9ef507c6aa5f6f13d
alnah <alexis.nahan@gmail.com>
2024-07-23 21:12:00 -0300

    feat(products): add error handling logic to `uploadProductImage` for files in uploads controller

--- src/controllers/uploadsController.js
+++ src/controllers/uploadsController.js
@@ -2,9 +2,26 @@ const path = require("path");
 const fs = require("fs");
 
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
+
+const MAX_SIZE = 1024 * 1024;
 
 const uploadProductImage = async (req, res, next) => {
+  if (!req.files) {
+    throw new BadRequestError("No file uploaded.");
+  }
+
   const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new BadRequestError("Please upload an image.");
+  }
+
+  if (productImage.size > MAX_SIZE) {
+    throw new BadRequestError(
+      `Please upload image smaller than ${MAX_SIZE / (1024 * 1024)}MB.`
+    );
+  }
 
   const uploadsDir = path.join(__dirname, "../../public/uploads");
   const imagePath = path.join(uploadsDir, productImage.name.toLowerCase());
